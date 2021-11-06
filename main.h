5c3a012bf63810858e78866bbc60a4d547a435f8
Felipe Rivas <fe.rivasb@gmail.com>
2021-11-06 17:38:23 +0000

    dumping all files from the final repo - Felipe 1

---
+++ main.h
@@ -0,0 +1,14 @@
+fndef _MAIN_H
+#define _MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+
+typedef struct my_printf
+{
+	char *name;
+	void (*f)(va_list);
+}print_t;
+
+int _printf(const char *format, ...);
+
+#endif
