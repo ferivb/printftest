916d7116a4f977759c7701a55154b4f1e5b0265d
Felipe Rivas <fe.rivasb@gmail.com>
2021-11-06 23:41:48 +0000

    main first draft incomplete

---
+++ _printf.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  *
+  */
+int _printf(const char *format, ...)
+{
+	va_list list;
+
+	print_t print[] = {
+		{"c", format_char};
+		{"s", format_string};
+		{"%", format_percent};
+		{NULL, NULL}};
+
+	int i = 0;
+
+	for (; f[i] != '\0'; i++)
+	{
+		if (f[i] != '%')
+		{
+			_write(f[i]);
+		}
+		else if (f[i + 1] != '%')
+		{
+
