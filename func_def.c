40bd5590f5953ee6901f8f75e5e24f148b0ffc79
Ana Rocha <anita.rocha0505@gmail.com>
2021-11-06 18:10:23 -0500

    added the func_def.c

--- func_def.c
+++ func_def.c
@@ -1,7 +1,49 @@
-#include <unistd.h>
+#include "main.h"
 
-void format_char(va_list c)
+/**
+ * format_char - Prints a char.
+ * @c: list of arguments.
+ * Return: The amount of chars entered.
+ */
+
+int format_char(va_list c)
 {
-    write(1, &c, 1);
+	_write(va_arg(c, int));
+	return (1);
 }
 
+/**
+ * format_string - Prints a string
+ * @s: list of arguments
+ * Return: The string.
+ */
+
+int format_string(va_list s)
+{
+	int i = 0;
+	char *s;
+
+	s = (va_arg(s, char*))
+
+	if (s == NULL)
+		s = "(null)";
+	for (; s[i] != '\0'; i++)
+	{
+		_write(s[i]);
+	}
+	return (i);
+}
+
+/**
+ * format_percent - Prints a porcent
+ * @percent: List of arguments
+ * Return: the string
+ */
+
+int format_percent(va_list percent)
+{
+	void(percent);
+
+	_write('%');
+	return (1);
+}
