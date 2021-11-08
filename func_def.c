96aa899e118cf76f88d0578950040d657c2cabdd
Felipe Rivas <fe.rivasb@gmail.com>
2021-11-08 16:59:14 +0000

    func for %d and %i added to func_def

--- func_def.c
+++ func_def.c
@@ -47,3 +47,36 @@ int format_percent(va_list percent)
 	_write('%');
 	return (1);
 }
+
+/**
+  * format_decimal - prints decimal and ints
+  * @num: number provided
+  * Return: amount of chars added
+  */
+int format_decimal(va_list num)
+{
+	int i = 0, dec_chars = 0;
+	char holder[1024];
+
+	if (num == 0)
+	{
+		_write('0');
+		return (dec_chars = 0);
+	}
+	if (num < 0)
+	{
+		_write('-');
+		dec_chars += 1;
+		num = num * 1;
+	}
+	for (; num >= 1; dec_chars++, i++)
+	{
+		holder[i] = ((num % 10) + 48);
+		num = num / 10;
+	}
+	for (i = i - 1; i > -1; i--)
+	{
+		_write(holder[i]);
+	}
+	return (dec_chars);
+}
