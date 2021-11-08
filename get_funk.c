2456dbd07585a8f86b1196ec89e73b8c88e94fdd
Ana Rocha <anita.rocha0505@gmail.com>
2021-11-08 17:09:05 -0500

    Update the get funk with the correct output

--- get_funk.c
+++ get_funk.c
@@ -1,33 +1,41 @@
 #include "main.h"
 
 /**
-  * get_funk - gets the fuctions according to the va_list
-  */
+ * get_funk - gets the fuctions according to the va_list
+ * @format: format
+ * @spec: specifier
+ * @info: list
+ * Return: The amount of arguments
+ */
 int get_funk(const char *format, print_s spec[], va_list info)
 {
 	int i, j, result, final_print;
 
 	final_print = 0;
-	/*Check every char of format*/
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format[i] != '\0'; i++) /*Check every char of format*/
 	{
-		/*Searches for '%'*/
-		if (format[i] == '%')
-		{
-			/*Iterates through the list of funks*/
+		if (format[i] == '%') /*Searches for '%'*/
+		{	/*Iterates through the list of funks*/
 			for (j = 0; spec[j].name != NULL; j++)
-			{
-				/*Compares the char next to '%' with the specs*/
+			{	/*Compares the char next to '%' with the specs*/
 				if (format[i + 1] == spec[j].name[0])
-				{
-					result = spec[j].f(info);
+				{	result = spec[j].f(info);
 					if (result == -1)
-					{
 						return (-1);
-					}
 					final_print += result;
 					break;
 				}
+			}
+			if (spec[j].name == NULL) /** Check for special cases */
+			{
+				if (format[i + 1] != ' ' && format[i + 1] != '\0')
+				{
+					_write(format[i]);
+					_write(format[i + 1]);
+					final_print += 2;
+				}
+				else
+					return (-1);
 			}
 			i += 1;
 		}
--- main.c
+++ main.c
@@ -9,15 +9,38 @@
  */
 int main(void)
 {
-    
-    _printf("Copiamos esta chimbada\n");
-    printf("somos bobos\n");
+    char a[] = "Hola";
+    int len;
+    int len2;
+
 
     _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
 
     _printf("String:[%s]\n", "1234");
     printf("String:[%s]\n", "1234");
+
+    _printf("string vacia %s\n","");
+    printf("string vacia %s\n", "");
+
+    _printf("Percent 3:[%%%]\n");
+    /*printf("Percent 3:[%%%]\n");*/
+
+    _printf("%sc%1\n");
+    printf("%sc%1\n");
+
+    _printf("%s\n", a);
+    printf("%s\n", a);
+
+    _printf("nulo %\n");
+    printf("nulo %\n");
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+
 
     _printf("Percent:[%%]\n");
     printf("Percent:[%%]\n");
