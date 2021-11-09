fd72a863f9111277bffb9dfd8a28af7d93d3ba17
Ana Rocha <anita.rocha0505@gmail.com>
2021-11-09 12:52:30 -0500

    correction

--- main.c
+++ main.c
@@ -9,47 +9,18 @@
  */
 int main(void)
 {
-    char a[] = "Hola";
-    int len;
-    int len2;
+	int a, b, c;
+	char *string;
 
+	a = 10;
+	b = 20;
+	c = -50;
+	string = "Hello world!";
 
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
+	_printf("This is my string: %s\n", string);
+	_printf("This is a [%d], and this is b [%d]\n", a, b);
+	_printf("This is c %d, and c is negative\n", c);
+	_printf("This is a single Character:[%c]\n", 'J');
 
-    _printf("String:[%s]\n", "1234");
-    printf("String:[%s]\n", "1234");
-
-    _printf("string vacia %s\n","");
-    printf("string vacia %s\n", "");
-
-    _printf("Percent 3:[%%%]\n");
-    /*printf("Percent 3:[%%%]\n");*/
-
-    _printf("%sc%1\n");
-    printf("%sc%1\n");
-
-    _printf("%s\n", a);
-    printf("%s\n", a);
-
-    _printf("nulo %\n");
-    printf("nulo %\n");
-
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-
-
-    _printf("Percent:[%%]\n");
-    printf("Percent:[%%]\n");
-
-    _printf("Number with d:[%d]\n", 1234);
-    printf("Number with d:[%d]\n", 1234);
-
-     _printf("Number with i:[%i]\n", 1234);
-    printf("Number with i:[%i]\n", 1234);
-   
     return (0);
 }
